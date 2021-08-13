5fe77c2a6c1b0881eb76b3590adde0c821367cd5
Sebastian N <sebas.a.nunez@gmail.com>
2021-08-12 21:19:51 -0400

    test

--- src/pages/Callback.js
+++ src/pages/Callback.js
@@ -6,13 +6,12 @@ function Callback() {
   const code = new URLSearchParams(queryParams).get("code");
   const client_id = process.env.REACT_APP_client_id;
   const client_secret = process.env.REACT_APP_client_secret;
-  const redirect_uri = "https://your-repositories.dexco.cl/me";
 
   useEffect(() => {
     const getToken = async () => {
       let respose = await fetch(
         "https://github.com/login/oauth/access_token" +
-          new URLSearchParams({ client_id, client_secret, code, redirect_uri })
+          new URLSearchParams({ client_id, client_secret, code })
       );
       respose = await respose.json();
       console.log(respose)
